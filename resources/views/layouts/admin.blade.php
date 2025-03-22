38789d83476b8c0210c5a2b8a6d512cc40cb22d0
dhyan14 <dhyanjain341988@gmail.com>
2025-03-22 10:31:23 +0530

    sss

--- resources/views/layouts/admin.blade.php
+++
@@ -1,6 +0,0 @@
-<li class="nav-item">
-    <a class="nav-link" href="{{ route('admin.assignments.index') }}">
-        <i class="fas fa-file-pdf"></i>
-        <span>Assignments</span>
-    </a>
-</li> 
